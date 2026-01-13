e503fda431cd8102c237bb19f14eb2dea6973151
Max de Groot <max@maximumfx.nl>
2026-01-13 10:14:24 +0100

    chore(perforce): refactor REST stub and improve changelist handling

--- client/ayon_perforce/backend/rest_stub.py
+++ client/ayon_perforce/backend/rest_stub.py
@@ -1,4 +1,5 @@
 """Controller for the changes viewer."""
+
 from __future__ import annotations
 
 import os
@@ -18,9 +19,7 @@ class PerforceRestStub:
     """Perforce REST API stub."""
 
     @staticmethod
-    def _wrap_call(
-            command: str,
-            **kwargs: Union[str, int, pathlib.Path]) -> dict:
+    def _wrap_call(command: str, **kwargs: Union[str, int, pathlib.Path]) -> dict:
         """Wrap the call to the Perforce REST API.
 
         Args:
@@ -41,9 +40,7 @@ class PerforceRestStub:
 
         action_url = f"{webserver_url}/perforce/{command}"
 
-        response = requests.post(
-            action_url, json=kwargs, timeout=10
-        )
+        response = requests.post(action_url, json=kwargs, timeout=10)
         if not response.ok:
             log.debug(response.content)
             raise RuntimeError(response.text)
@@ -60,16 +57,11 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "is_in_any_workspace", path=path)
+        return PerforceRestStub._wrap_call("is_in_any_workspace", path=path)
 
     @staticmethod
     def login(
-        host: str,
-        port: int,
-        username: str,
-        password: str,
-        workspace_name: str
+        host: str, port: int, username: str, password: str, workspace_name: str
     ) -> dict:
         """Login to Perforce server.
 
@@ -90,7 +82,8 @@ class PerforceRestStub:
             port=port,
             username=username,
             password=password,
-            workspace_name=workspace_name)
+            workspace_name=workspace_name,
+        )
 
     @staticmethod
     def add(path: Union[str, pathlib.Path], comment: str = "") -> dict:
@@ -104,8 +97,7 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "add", path=path, comment=comment)
+        return PerforceRestStub._wrap_call("add", path=path, comment=comment)
 
     @staticmethod
     def sync_latest_version(path: Union[str, pathlib.Path]) -> dict:
@@ -118,8 +110,7 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "sync_latest_version", path=path)
+        return PerforceRestStub._wrap_call("sync_latest_version", path=path)
 
     @staticmethod
     def sync_to_version(path: Union[str, pathlib.Path], version: int) -> dict:
@@ -134,7 +125,8 @@ class PerforceRestStub:
 
         """
         return PerforceRestStub._wrap_call(
-            "sync_to_version", path=path, version=version)
+            "sync_to_version", path=path, version=version
+        )
 
     @staticmethod
     def checkout(path: Union[str, pathlib.Path], comment: str = "") -> dict:
@@ -148,8 +140,7 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "checkout", path=path, comment=comment)
+        return PerforceRestStub._wrap_call("checkout", path=path, comment=comment)
 
     @staticmethod
     def is_checkouted(path: Union[str, pathlib.Path]) -> dict:
@@ -162,8 +153,7 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "is_checkouted", path=path)
+        return PerforceRestStub._wrap_call("is_checkouted", path=path)
 
     @staticmethod
     def get_last_change_list() -> dict:
@@ -176,14 +166,29 @@ class PerforceRestStub:
         return PerforceRestStub._wrap_call("get_last_change_list")
 
     @staticmethod
-    def get_changes() -> dict:
+    def get_changes() -> list[dict]:
         """Get changes from Perforce.
 
         Returns:
-            dict: Changes from Perforce
+            list[dict]: Changes from Perforce
 
         """
-        return PerforceRestStub._wrap_call("get_changes")
+        result = PerforceRestStub._wrap_call("get_changes")
+        if not isinstance(result, list):
+            if not result:
+                return []
+            result = [result]
+        return result
+
+    @staticmethod
+    def get_uncommitted_changes() -> dict:
+        """Get the uncommitted changes.
+
+        Returns:
+            dict: Response from the server.
+        """
+        response = PerforceRestStub._wrap_call("get_uncommitted_changes")
+        return response
 
     @staticmethod
     def submit_change_list(comment: str) -> dict:
@@ -196,8 +201,7 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "submit_change_list", comment=comment)
+        return PerforceRestStub._wrap_call("submit_change_list", comment=comment)
 
     @staticmethod
     def exists_on_server(path: Union[str, pathlib.Path]) -> dict:
@@ -207,8 +211,29 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "exists_on_server", path=path)
+        return PerforceRestStub._wrap_call("exists_on_server", path=path)
+
+    @staticmethod
+    def submit_default_changelist(comment):
+        """Submit the default change list with a comment.
+
+        Args:
+            comment (str): Comment for the change list.
+        """
+        response = PerforceRestStub._wrap_call(
+            "submit_default_changelist", comment=comment
+        )
+        return response
+
+    @staticmethod
+    def revert(path):
+        """Revert specified file for path.
+
+        Args:
+            path (str): Path to the file.
+        """
+        response = PerforceRestStub._wrap_call("revert", path=path)
+        return response
 
     @staticmethod
     def get_stream(workspace_name: str) -> dict:
@@ -221,8 +246,7 @@ class PerforceRestStub:
             dict: Response from the server.
 
         """
-        return PerforceRestStub._wrap_call(
-            "get_stream", workspace_name=workspace_name)
+        return PerforceRestStub._wrap_call("get_stream", workspace_name=workspace_name)
 
     @staticmethod
     def get_workspace_dir(workspace_name: str) -> dict:
@@ -236,4 +260,5 @@ class PerforceRestStub:
 
         """
         return PerforceRestStub._wrap_call(
-            "get_workspace_dir", workspace_name=workspace_name)
+            "get_workspace_dir", workspace_name=workspace_name
+        )
